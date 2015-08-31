64df633e68e660facaf4ca93054041a29d0d67d8
Kenneth Corea <kcorea@hawaii.edu>
2015-08-31 12:07:55 -1000

    linked bootstrap

--- pln/pln/settings/development.py
+++ pln/pln/settings/development.py
@@ -2,14 +2,11 @@
 Django settings for pln project.
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from unipath import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+# points to project base directory
+PROJECT_DIR = PROJECT_DIR = Path(__file__).ancestor(3)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -48,7 +45,7 @@ ROOT_URLCONF = 'pln.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [PROJECT_DIR.child('templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,33 +60,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pln.wsgi.application'
 
+# databases
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+##DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'plndb',
+#        'USER': 'djangodbuser',
+#        'PASSWORD': '1',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#    }
+#}
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Pacific/Honolulu'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (PROJECT_DIR.child('static'),)
